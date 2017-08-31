eeece1f863b9b6c1c2c2cfe1175d1852ee0ea138
Thach Nguyen <pcngocthach@gmail.com>
2017-08-31 10:39:04 +0700

    - Remove odo unsuage

--- app/index.js
+++ app/index.js
@@ -5,27 +5,6 @@ import {
   View
 } from 'react-native';
 
-import Odoo from'./odoo/index';
-
-var odoo = new Odoo({
-  host: 'localhost',
-  port: 4569,
-  database: '4yopping',
-  username: 'admin',
-  password: '4yopping'
-});
-
-// Connect to Odoo
-odoo.connect(function (err) {
-  if (err) { return console.log(err); }
-
-  // Get a partner
-  odoo.get('res.partner', 4, function (err, partner) {
-    if (err) { return console.log(err); }
-
-    console.log('Partner', partner);
-  });
-});
 export default class OdooMobile extends Component {
   render() {
     return (
